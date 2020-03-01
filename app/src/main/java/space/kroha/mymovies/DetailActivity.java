bee3b898cd114e8ded204b6be73a44ea90a9a47e
Дмитрий <dimkakrohin@mail.ru>
2020-03-01 21:30:48 +0300

    Initial commit

---
+++ app/src/main/java/space/kroha/mymovies/DetailActivity.java
@@ -0,0 +1,14 @@
+package space.kroha.mymovies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
